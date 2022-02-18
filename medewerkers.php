7e0626666b16a3b53c6801fb9c3a32fa51f88857
DannyNguyen0411 <90318109+DannyNguyen0411@users.noreply.github.com>
2022-02-18 11:35:40 +0100

    3 nieuwe mederwerkers

--- medewerkers.php
+++ medewerkers.php
@@ -1,6 +1,52 @@
-<?php
-    echo "<h1>Redirecting request</h1>";
-	header('Refresh: 2; url=nogniet.php');	
-    exit(); 
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Onze Mederwerkers</title>
+    <link rel="stylesheet" type="text/css" href="company.css">
+</head>
+<body>
+    <header>
+    <img src="images/Logo.jpg" alt="logo">
+        <!-- hieronder wordt het menu opgehaald. -->
+        <?php
+			include "nav.html";
+		?>
+    </header>
+    <main>
+    <h1>Onze mederwerkers</h1>
+        <section>
+            <article>
+    <p>Natuurlijk moeten we mederwerkers hebben om te kunnen werken.<br>
+       We hebben hier ook iedereen uit verschillende leeftijden.</p>
+       <table>
+           <tr>
+               <td>
+        <img src="images/Toad.jpg" alt="Medewerker" width="200" height="200">
+        <p class="color"><strong>Toad</strong><br>
+        is een werknemer. Hij had geen zin meer om ee werkgever te zijn,<br>
+         dus daarom is iemand anders de werkgever.<br>
+        Toad importeerd de dranken en hij is ook tegelijkertijd de proever van de dranken,<br>
+        want hij moet proeven of het wel goed is en hij houdt ook van drinken(vooral alcohol).</p>
+        </td>
+        <td>
+        <img src="images/Toadny kopiëren.jpg" alt="Medewerker" width="200" height="200">
+        <p><strong>Toadny</strong><br>
+         is ook een werknemer. Hij importeerd alle producten en verzin ook de prijs<br>
+        hoe duur een product moet zijn. Hij drinkt geen alcohol en is goed met verkopen en importeren van producten.</p>
+        </td>
+        <td>       
+       <img src="images/Among-Us-Logo.jpg" alt="Werkgever" width="200" height="200">
+        <p><strong>Among us</strong><br>
+        is erg sus. Daarom is hij de nieuwe werkgever. Hij bepaalt in welk tijd de producten geïmporteerd wordt,<br>
+        welke bedragen het opleverd en hoeveel tijd het kost. Hij is erg slim en goed in inversteren, maar hij is ook een beetje gierig.</p>
+        </td>
+        </tr>
+        </table>                 
+       </article>
+       </section>
+       </main>
+</body>
+</html>
