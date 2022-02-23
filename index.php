3ecff544298e2a23e9b6e2537069805a12d58724
Robin <robinpeereboom04@gmail.com>
2022-02-23 10:29:08 +0100

    inlog klant

--- index.php
+++ index.php
@@ -16,7 +16,22 @@
 	</header>
  
 	<!-- op de home pagina wat enthousiaste tekst over het bedrijf en de producten -->
- 	<main>	
+ 	<main>
+
+<p>Login</p>
+
+<form action="inlogklant.php" method="POST">
+
+<input type="text" id="user" name="username" placeholder="username"/><br><br>
+<input type="text" id="pass" name="password" placeholder="password"/><br><br>
+<button type="submit" id="btn" name="login" default>login</button>
+
+</form>
+
+<a href="inlogklant.php"><div style="float:right"><button>logout</button></div></a>
+
+    <h1>Welkom bij de Login page voor klant!</h1>
+
 		 <a href="https://play.pokemonshowdown.com/">
 		  <img class="centreer" src="images/homepagefotoappelsap.nl.jpg" alt="main page image" width="1000px" height="600px"> 
 		  </a>
@@ -33,7 +48,6 @@
 		  </p>
 		  <p>
 			  <?php
-			  // in onderstaande php source worden 6 producten op scherm getoond
 			  include "zesproducten.php";
 			  ?>
 		  </p>
--- inlogklant.php
+++ inlogklant.php
@@ -1,6 +1,55 @@
-<?php
-    echo "<h1>Redirecting request</h1>";
-	header('Refresh: 2; url=nogniet.php');	
-    exit(); 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+<body>
+<?php session_start();
+
+include('dbconnect.php');
+
+if(isset($_POST['login']));
+{
+    $user_unsafe=$_POST['username'];
+    $pass_unsafe=$_POST['password'];
+
+    $user = mysqli($con,$user_unsafe);
+    $pass = mysqli($con,$pass_unsafe);
+
+    $query=mysqli_query($con,"mysql:host=localhost;dbname=appelsapgenieters', 'root' ,'root'")or die(mysqli_error($con));
+
+    $row=mysqli_fetch_array($query);
+
+         $name=$row['username'];
+         $counter=mysqli_num_rows($query);
+         $id=$row['id'];
+
+         if ($counter == 0)
+         {
+            echo "<script type='text/javascript'>alert('Invalid Usrename or Password!');
+            document.location='login.php'</script>";
+         }
+         else
+         {
+            $_SESSION['id']=$id;
+            $_SESSION['username']=$name;
+
+            echo "<script type='text/javascript'>document.location='index.php'</script>";
+         }
+
+}
+
+include('database dbconnect .php');
+     session_destroy();
+
+     echo '<meta http-equiv="refresh" content="2;url=login.php">';
+     echo '<progress max=100><strong>Progress: 60% done.</strong></progress><br>';
+     echo '<span class="itext">Logging out please wait!...</span>';
 
 ?>
+   
+</body>
+</html>
